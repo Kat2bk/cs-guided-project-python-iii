f26c5f76fe8ab301e51b268768c548986c42438d
Katherine Kumar <Katherine2bk@gmail.com>
2021-10-14 08:51:07 -0700

    added more notes

--- src/notes.py
+++ src/notes.py
@@ -11,8 +11,22 @@
 
 # does the number of steps change? No. It's one step. Constant => It's O(1)
 
-def print_many_items(n):
-    for i in range(n):
-        print(i)
+# def print_many_items(n):
+#     for i in range(n):
+#         print(i)
 
-print(print_many_items(5))
+# print(print_many_items(5))
+# print(print_many_items(50))
+
+# the amount of steps increase by the amount of input. Linear => O(n)
+
+# def print_many_items_twice(n):
+#     for i in range(n):
+#         print(i)
+#         print(i)
+
+# the amount of steps has doubled despite the input. O(2n). It's still linear time.
+# There isn't much of a difference between these last two functions, we only care
+# about the class (linear) time.
+
+
